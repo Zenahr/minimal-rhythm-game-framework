b58b7974d381bd520a6923d7e68f1e828e0b7b31
Zenahr Barzani <zenmatica@gmail.com>
2020-05-16 23:54:34 +0200

    added sound assets

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ def play_song(song):
     """This should play basic sounds corresponding to the characters of the string.
     The song will be repeated infinetely. The length of the song is defined by the song itself."""
     for note in song:
-        print(note)
+        if(note == "1"):
+            play("base-drum")
+        if(note == "0"):
+            play("snare-drum")
+        if(note == "-"):
+            play("nothing")
+
+def play(instrument):
+    if(instrument == "base-drum"):
+        print("base-drum")
+    elif(instrument == "snare-drum"):
+        print("snare-drum")
+    elif(instrument == "nothing"):
+        print("nothing")
 
 play_song(song_1)
